e5f616e4f9d7efbc462aa22be218a4f49e35b3b0
gabrielgds <gabrielgaldino7@yahoo.com>
2018-10-24 16:26:50 -0300

    Estabelece troca de pacotes entre servidor e cliente

--- client.py
+++ client.py
@@ -2,47 +2,42 @@ from carca_socket import CarcaSocket
 from carca_packet import CarcaPacket
 from random import randint
 from time import sleep
+import utils
 
 
 class CarcaClient():
-    def __init__(self, carca_socket, mss=2):
+    def __init__(self, carca_socket, data, mss=2):
         self._socket = carca_socket
         self._last_byte_read = 0
+        self._send_base = 0
+        self._data = data
+        self._last_packet_sent = None
         self._mss = mss
         self._byte_list = []
+        
+    def listen(self, client_address):
+        self._socket.bind(client_address)
+       
+    def send_segment(self, server_address):
+        
+        byte_string = utils.serialize_to_string(self._data)
+        segments_list = utils.string_to_segments(byte_string, self._mss)
 
-    def request_data(self, server_address):
-        client_packet = CarcaPacket(ack_number=0,seq_number=randint(0, 1000), SYN=1, mss=self._mss)
-        #print(client_packet._seq_number)
-        #print(client_packet._ack_number)
-        while True:
-            self._socket.sendto(client_packet, server_address)
+        packet = CarcaPacket(seq_number=1, ack_number=0, payload=segments_list[0])
+        self._send_base = packet._seq_number
+        self._socket.sendto(packet, server_address)
+        
+        i = 0
+        while i < len(segments_list) - 1:
             server_packet, _ = self._socket.recvfrom(4096)
-            print("Recebi primeiro pacote")
-            if server_packet._ack_number > self._last_byte_read:
-                self._byte_list.append(server_packet._payload)
-                self._last_byte_read = server_packet._ack_number
-                #print(server_packet._seq_number)
-                #print(server_packet._ack_number)
-                confirm_packet = CarcaPacket(ack_number=server_packet._seq_number + self._mss,
-                                             seq_number=server_packet._ack_number, SYN=0)
-                #print(confirm_packet._SYN)
-                self._socket.sendto(confirm_packet, server_address)
-                #print(confirm_packet._seq_number)
-                #print(confirm_packet._ack_number)
-                break
-
-    def receive_data(self):
-        #byte_list = []
-        while True:
-            server_packet, server_address = self._socket.recvfrom(4096)
-            print("cheguei aqui")
-            data = server_packet._payload   
-            if server_packet._ack_number > self._last_byte_read:
-                self._last_byte_read = server_packet._ack_number
-                self._byte_list.append(data)
-                confirm_packet = CarcaPacket(ack_number=server_packet._seq_number + self._mss,
-                                             seq_number=server_packet._ack_number)
+            if server_packet._ack_number > self._send_base:
+                self._send_base = server_packet._ack_number
+                i += 1
+            #packet = CarcaPacket(seq_number=server_packet._ack_number, ack_number=server_packet._seq_number + 1, payload=segments_list[i])
+            if i == len(segments_list) - 1:
+                packet = CarcaPacket(seq_number=server_packet._ack_number, FIN=1, 
+                                    ack_number=server_packet._seq_number + 1, payload=segments_list[i])
             else:
-                confirm_packet = CarcaPacket(ack_number=self._last_byte_read)
-            self._socket.sendto(confirm_packet, server_address)
+                 packet = CarcaPacket(seq_number=server_packet._ack_number, 
+                                    ack_number=server_packet._seq_number + 1, payload=segments_list[i])
+            self._socket.sendto(packet, server_address)          
--- server.py
+++ server.py
@@ -2,48 +2,38 @@ from carca_socket import CarcaSocket
 from carca_packet import CarcaPacket
 import utils
 from random import randint
+from time import sleep
 
 
 class CarcaServer():
-    def __init__(self, data, carca_socket):
+    def __init__(self, carca_socket, data, mss=2):
         self._data = data
         self._socket = carca_socket
         self._send_base = 0
+        self._last_byte_read = 0
+        self._last_packet_received = None
+        self._byte_list = []
+        self._mss = mss
         
     def listen(self, server_address):
         self._socket.bind(server_address)
-        
-    def accept_cnn(self):
+            
+    def receive_data(self):
         while True:
             client_packet, client_address = self._socket.recvfrom(4096)
-            if client_packet._SYN == 1:
-                byte_string = utils.serialize_to_string(self._data)
-                segment_list = utils.string_to_segments(
-                    byte_string, client_packet._mss)
-                packet = CarcaPacket(seq_number=randint(0, 1000),
-                                     ack_number=client_packet._seq_number + 1,
-                                     mss=client_packet._mss, payload=segment_list[0])
-                #print(packet._seq_number)
-                #print(packet._ack_number)
-                self._socket.sendto(packet, client_address)
-            else:
-                print("recebi SYN 0")
-                segment_list.pop(0)
-                self._send_base = client_packet._ack_number
-                self.send_segment(segment_list, client_packet, client_address)
-                break
+            if client_packet._seq_number > self._last_byte_read:
+                self._last_byte_read = client_packet._seq_number
+                self._byte_list.append(client_packet._payload)
+                self._last_packet_received = client_packet
+                
+                confirm_packet = CarcaPacket(seq_number=client_packet._ack_number,
+                                     ack_number=client_packet._seq_number + client_packet._mss)
 
-    def send_segment(self, seg_list, to_send_packet, client_address):
-        self._socket.sendto(to_send_packet, client_address)
-        while True:
-            #client_packet, _ = self._socket.recvfrom(4096)
-            for seg in seg_list:
-                client_packet, _ = self._socket.recvfrom(4096)
-                while client_packet._ack_number <= self._send_base:
-                    #packet = CarcaPacket(seq_number=client_packet._ack_number,
-                    #                     ack_number=client_packet._seq_number + 1)
-                    self._socket.sendto(client_packet, client_address)
-                self._send_base = client_packet._ack_number
-                to_send_packet = CarcaPacket(seq_number=client_packet._ack_number,
-                           ack_number=client_packet._seq_number + 1)
-                self._socket.sendto(to_send_packet, client_address)
+                self._socket.sendto(confirm_packet, client_address)
+            else:
+                confirm_packet = CarcaPacket(seq_number=self._last_packet_received._ack_number,
+                                     ack_number=self._last_packet_received._seq_number + client_packet._mss)
+                self._socket.sendto(confirm_packet, client_address)
+            if client_packet._FIN == 1: 
+                return self._byte_list
+                break
